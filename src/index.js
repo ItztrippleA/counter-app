b327cf80fbda21047fef14bb09f8fe747e8fa898
Abdullahi Adam <AbdullahiAdam.aa@gmail.com>
2020-12-16 06:05:08 +0100

    Updating state

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+//import App from './App';
 import reportWebVitals from './reportWebVitals';
 import "bootstrap/dist/css/bootstrap.css";
 import Counter from "./components/counter";
 
+
 ReactDOM.render(<Counter />, document.getElementById('root'));
-console.log("Hello niaja");
+//console.log("Hello niaja");
 reportWebVitals();
