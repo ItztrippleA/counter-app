fe44c86d82f1775ab89053b5af49e45d1ffcd5dc
Abdullahi Adam <AbdullahiAdam.aa@gmail.com>
2020-12-30 02:13:35 +0100

    object destructuring update

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -3,14 +3,16 @@ import Counter from './counter';
 
 class Counters extends Component {
  
+    
     render() { 
+        const { onReset, onIncreament,onDelete, counters}= this.props;
         return ( 
         
         <div>
-            <button onClick={this.props.onReset} className="btn btn-primary btn-sm m-2">Reset</button>
-            {this.state.counters.map(counter => 
+            <button onClick={onReset} className="btn btn-primary btn-sm m-2">Reset</button>
+            {counters.map(counter => 
                 (
-                <Counter key={counter.id } onIncreament={this.props.onIncreament} onDelete = {this.onDelete} counter = {counter}/>))}
+                <Counter key={counter.id } onIncreament={onIncreament} onDelete = {onDelete} counter = {counter}/>))}
         </div> );
     }
 }
--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 
 
-const NavBar = (props)=>{
+const NavBar = ({totalCounters})=>{
     return (  
         <nav className="navbar navbar-light bg-light">
             <div className="container-fluid">
                  <span className="badge badge-pill badge-secondary ">
-                     {props.totalCounters}
+                     {totalCounters}
                      </span>    
             </div>
         </nav>
