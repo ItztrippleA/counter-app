8c824d1e63b3b1da1edf4119d01815c7f92c51c6
Abdullahi Adam <AbdullahiAdam.aa@gmail.com>
2020-12-30 02:03:28 +0100

    statelss functional component update

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
-class NavBar extends Component {
-    
-    render() { 
-        return (  
-            <nav className="navbar navbar-light bg-light">
-                <div className="container-fluid">
-                     <span className="badge badge-pill badge-secondary ">{this.props.totalCounters}</span>    
-                </div>
-            </nav>
-        );
-    }
-}
- 
+
+
+const NavBar = (props)=>{
+    return (  
+        <nav className="navbar navbar-light bg-light">
+            <div className="container-fluid">
+                 <span className="badge badge-pill badge-secondary ">
+                     {props.totalCounters}
+                     </span>    
+            </div>
+        </nav>
+    );
+
+}; 
 export default NavBar;
